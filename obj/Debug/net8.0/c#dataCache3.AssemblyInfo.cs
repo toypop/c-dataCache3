3546edf531c20d6aafabaa59b1d71b5ff8b0aad3
AlessandroC <p4cco22@gmail.com>
2025-08-15 23:18:24 +0200

    messo menu hamburger  nella landing page

--- obj/Debug/net8.0/c#dataCache3.AssemblyInfo.cs
+++ obj/Debug/net8.0/c#dataCache3.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#dataCache3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66192286fe21de04dd04ff1b01baf39270ef1e33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cf4161e8cdc85a9675643550a72ceb1704a2916")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#dataCache3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#dataCache3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
