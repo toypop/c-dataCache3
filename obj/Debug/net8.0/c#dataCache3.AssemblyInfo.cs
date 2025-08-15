9290f83741c8dee010d1d3771dc213bb0305e425
AlessandroC <p4cco22@gmail.com>
2025-08-16 01:22:20 +0200

    corretto autenticazione 2 per sicurezza

--- obj/Debug/net8.0/c#dataCache3.AssemblyInfo.cs
+++ obj/Debug/net8.0/c#dataCache3.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#dataCache3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cf4161e8cdc85a9675643550a72ceb1704a2916")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e47b6952d10619ed1c0d61d1a4e733102c9a1db6")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#dataCache3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#dataCache3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
