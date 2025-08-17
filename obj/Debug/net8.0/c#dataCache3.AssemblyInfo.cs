4114f8ff850576a3f5d5d77933f38070c830f866
AlessandroC <p4cco22@gmail.com>
2025-08-17 02:02:11 +0200

    corretto reload pagina dopo inserimento api keys

--- obj/Debug/net8.0/c#dataCache3.AssemblyInfo.cs
+++ obj/Debug/net8.0/c#dataCache3.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#dataCache3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8feed0dc1cb9d845c66b0dc14a8681a97df98fde")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23378f88195c4098c6fc3827b6de2635a4210bfa")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#dataCache3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#dataCache3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
